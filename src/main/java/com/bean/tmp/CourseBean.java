0d9990c0d2a00f62321615620aee19b8b7f0ac64
tejas <tejasshah@gmail.com>
2022-08-12 16:44:09 +0530

    tmp bean for m to m

---
+++ src/main/java/com/bean/tmp/CourseBean.java
@@ -0,0 +1,50 @@
+package com.bean.tmp;
+
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class CourseBean {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	UUID courseId;
+
+	String courseName;
+
+	@ManyToMany(mappedBy = "courses")
+	Set<StudentBean> students;
+
+	public UUID getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(UUID courseId) {
+		this.courseId = courseId;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public Set<StudentBean> getStudents() {
+		return students;
+	}
+
+	public void setStudents(Set<StudentBean> students) {
+		this.students = students;
+	}
+
+}
